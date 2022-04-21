6c03821a7376dfbc93129653dfd29f6e60940a2f
Pavel <impetus@yandex.ru>
2022-04-21 13:08:20 +0300

    fixes 4

--- app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.java
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.java
@@ -1,14 +1,24 @@
 package ru.samsung.itschool.mdev.myapplication;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import ru.samsung.itschool.mdev.myapplication.api.AnekdotAPI;
 import ru.samsung.itschool.mdev.myapplication.model.Anekdot;
 
@@ -29,7 +39,29 @@ public class MainActivity extends AppCompatActivity {
         MyAdapter adapter = new MyAdapter(anekdots);
         recyclerView.setAdapter(adapter);
 
-        
+        Gson gson = new GsonBuilder().create();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        api = retrofit.create(AnekdotAPI.class);
+        api.getAnekdot("anekdot.ru",10,"new anekdot").enqueue(new Callback<ArrayList<Anekdot>>() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onResponse(Call<ArrayList<Anekdot>> call, Response<ArrayList<Anekdot>> response) {
+                if(response.code() == 200) {
+                    assert response.body() != null;
+                    anekdots.addAll(response.body());
+                    Objects.requireNonNull(recyclerView.getAdapter()).notifyDataSetChanged();
+                } else {
+                    Log.d("RRR","Response code:" + response.code());
+                }
+            }
+            @Override
+            public void onFailure(Call<ArrayList<Anekdot>> call, Throwable t) {
+                Log.d("RRR",t.getMessage());
+            }
+        });
 
     }
 }
